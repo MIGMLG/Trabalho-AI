6e406885903a453167b6efc497b783a1bd3b934e
DevMig <nerdxontheline@outlook.com>
2020-06-21 17:49:45 +0100

    Advanced Robot Test!

--- src/Robots/BasicAdvancedRobot.java
+++ src/Robots/BasicAdvancedRobot.java
@@ -1,4 +1,22 @@
 package Robots;
 
-public class BasicAdvancedRobot {
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+public class BasicAdvancedRobot extends AdvancedRobot {
+
+    @Override
+    public void run() {
+        while (true) {
+            this.setAhead(100);
+            this.setTurnRight(100);
+            this.scan();
+            execute();
+        }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(3);
+    }
 }
